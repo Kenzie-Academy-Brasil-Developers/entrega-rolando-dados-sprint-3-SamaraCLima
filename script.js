4470b740118b01feb18c6b18c85986d2e2263f6d
SamaraCLima <103124423+SamaraCLima@users.noreply.github.com>
2022-07-17 14:34:49 -0300

    s7-18

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
+let count = [];
+  for(i = 1; i<=1000;i++){
+      let somaDosDados = 0
+      somaDosDados = getRandomArbitrary()+ getRandomArbitrary()
+      if(count[somaDosDados]===undefined){
+        count[somaDosDados] = []
+    }
+    count[somaDosDados]++
+  }
+  console.log (count)
+function getRandomArbitrary() {
+    return Math.floor(Math.random() * (6 - 0) + 1)
+  }
+  let body = document.querySelector('body')
+  let div1 = document.createElement('div')
+  let ul = document.createElement('ul')
 
+  for(let i = 2 ; i<=12;i++){
+    let li = document.createElement('li')
+    li.innerText =  `${i} = ${count[i]} `
+    div1.appendChild(li)
+  }
+  body.appendChild(div1)
+  div1.appendChild(ul)
+
+  let result = document.createElement('div')
+  result.id = 'barragrafica'
+  body.style.minWidth = "360px"
+  body.style.minHeight = "500px"
+  
+  for(let i =2;i<=12;i++){
+    let div = document.createElement('div')
+    div.id = i
+    div.style.width = "45px"
+    div.style.height = ""+count[i]+"px"
+    div.style.display = "inline-block"
+    div.style.border = "5px solid"
+    div.style.background = "pink"
+
+    result.appendChild(div)
+  }
+  body.appendChild(result)
